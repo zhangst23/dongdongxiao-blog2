01c7cfd9aa359ab66949abf07ff2c571f63386cf
栋栋晓 <2243980491@qq.com>
2016-02-29 00:27:33 +0800

    create categorys

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   resources :lists
   resources :homes
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160228082928) do
+ActiveRecord::Schema.define(version: 20160228162655) do
+
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "homes", force: :cascade do |t|
     t.string   "index"
