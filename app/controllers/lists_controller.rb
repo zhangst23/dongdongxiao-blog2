878d2cc024cad26d9ce971f7c9ea2a59feafb1f1
栋栋晓 <2243980491@qq.com>
2016-02-29 00:26:34 +0800

    add list views

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,74 +1,103 @@
 class ListsController < ApplicationController
+  before_action :login_required, :no_locked_required, except: [:index, :show, :search]
+  before_action :find_list, only: [:edit, :update, :trash]
   before_action :set_list, only: [:show, :edit, :update, :destroy]
 
   # GET /lists
   # GET /lists.json
   def index
-    @lists = List.all
+    @lists = List.includes(:user, :category).page(params[:page])
+
+    if params[:category_id]
+      @category = Category.where('lower(slug) = ?', params[:category_id].downcase).first!
+      @lists = @lists.where(category: @category)
+    end
+
+    # Set default tab
+    unless %w(newest excellent).includes? params[:tab]
+      params[:tab] = 'newest'
+    end
+
+    case params[:tab]
+    when 'newest'
+      @lists = @lists.order(hot: :desc)
+    when 'excellent'
+      @lists = @lists.order(id: :desc)
+    end
   end
+
+  def search
+    @lists = List.search(
+      query: {
+        multi_match: {
+          query: params[:q].to_s,
+          fields: ['title', 'body']
+        }
+      },
+      filter: {
+        term: {
+          trashed: false
+        }
+      }
+    ).page(params[:page]).records
+  end
+
+
 
   # GET /lists/1
   # GET /lists/1.json
   def show
+    @list = List.find params[:id]
+
+    if params[:comment_id] and comment = @list.comments.find_by(id: params.delete(:comment_id))
+      params[:page] = comment.page
+    end
+
+    @comments = @list.comments.includes(:user).order(id: :asc).page(params[:page])
+
+    respond_to do |format|
+      format.html
+    end
   end
 
   # GET /lists/new
   def new
-    @list = List.new
+    @category = Category.where('lower(slug) = ?', params[:category_id].downcase).first if params[:category_id].present?
+    @list = List.new category: @category
   end
 
   # GET /lists/1/edit
   def edit
+
   end
 
   # POST /lists
   # POST /lists.json
   def create
-    @list = List.new(list_params)
-
-    respond_to do |format|
-      if @list.save
-        format.html { redirect_to @list, notice: 'List was successfully created.' }
-        format.json { render :show, status: :created, location: @list }
-      else
-        format.html { render :new }
-        format.json { render json: @list.errors, status: :unprocessable_entity }
-      end
-    end
+    @list = current_user.topics.create list_params
   end
 
   # PATCH/PUT /lists/1
   # PATCH/PUT /lists/1.json
   def update
-    respond_to do |format|
-      if @list.update(list_params)
-        format.html { redirect_to @list, notice: 'List was successfully updated.' }
-        format.json { render :show, status: :ok, location: @list }
-      else
-        format.html { render :edit }
-        format.json { render json: @list.errors, status: :unprocessable_entity }
-      end
-    end
+    @list.update_attributes list_params
   end
 
   # DELETE /lists/1
   # DELETE /lists/1.json
   def destroy
     @list.destroy
-    respond_to do |format|
-      format.html { redirect_to lists_url, notice: 'List was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_via_turbolinks_to lists_path
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_list
-      @list = List.find(params[:id])
+    def find_list
+      @list = current_user.lists.find params[:id]
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def list_params
-      params.fetch(:list, {})
+      params.require(:list).permit(:title, :category_id, :body)
     end
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,47 @@
 class List < ActiveRecord::Base
+
+	belongs_to :user
+	belongs_to :category, counter_cache: true
+	has_many :comments, as: 'commentable'
+
+	validates :title, :body, presence: true
+
+	after_creaet :update_hot, :owner_subscribe
+	after_touch :update_hot
+
+	def calculate_hot
+		order = Math.log10([comments_count, 1].max)
+		order + created_at.to_f / 45000
+	end
+
+	def update_hot
+		# reload because comments_count has been cache in associations
+		reload
+		update_attribute :hot, calculate_hot
+	end
+
+	def owner_subscribe
+		subscribe_by user
+	end
+
+	def total_pages
+		(comments_count.to_f / Comment.default_per_page).ceil
+	end
+
+	def more_like_this(num = 5)
+		List.search(
+			query: {
+				more_like_this: {
+					fields ['title', 'body'],
+					like_text: title + '\n' + body
+				}
+			},
+			filter: {
+				and: [
+					{ term: { trashed: false } },
+					{ not: { term: { id: id } } }
+				]
+			}
+		).limit(num).records.to_a rescue []
+	end
 end
