6a0eea3e2afe62603abde210f5acd3967741232e
栋栋晓 <2243980491@qq.com>
2016-02-28 12:20:40 +0800

    scaffold home index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :homes
   devise_for :users
   resources :posts
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160228041753) do
+ActiveRecord::Schema.define(version: 20160228042009) do
+
+  create_table "homes", force: :cascade do |t|
+    t.string   "index"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
