cab1c2fe003044f76c0f4fb2325b2f0da4f69e8d
栋栋晓 <2243980491@qq.com>
2016-02-28 16:27:32 +0800

    scaffold lists

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160228082544) do
+ActiveRecord::Schema.define(version: 20160228082642) do
 
   create_table "homes", force: :cascade do |t|
     t.string   "index"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "lists", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
