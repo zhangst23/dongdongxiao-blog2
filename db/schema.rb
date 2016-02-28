4f702693e7cc1de35c31c6226ab463cc9a4fea85
栋栋晓 <2243980491@qq.com>
2016-02-28 16:26:28 +0800

    add user_id to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160228042009) do
+ActiveRecord::Schema.define(version: 20160228082544) do
 
   create_table "homes", force: :cascade do |t|
     t.string   "index"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160228042009) do
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
